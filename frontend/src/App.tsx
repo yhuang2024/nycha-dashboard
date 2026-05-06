54e410c2528a24723c94873e6639852c309817d6
yhuang2024 <yuyuanesclassemail@gmail.com>
2026-05-06 11:34:28 -0400

    trying frontend deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -54,8 +54,8 @@ export default function App() {
   const [data, setData] = useState<NYCHARecord[]>([])
 
   useEffect(() => {
-    fetch("http://localhost:3001/getcsv?filepath=final_merged_nycha.csv&hasHeader=true")
-    //fetch("/api/getcsv?filepath=data/final_merged_nycha.csv&hasHeader=true")
+    //fetch("http://localhost:3001/getcsv?filepath=final_merged_nycha.csv&hasHeader=true")
+    fetch("https://nycha-dashboard-backend.vercel.app/getcsv?filepath=final_merged_nycha.csv&hasHeader=true")
       .then((res) => res.json())
       .then((json) => {
         const records = (json.data as any[])
